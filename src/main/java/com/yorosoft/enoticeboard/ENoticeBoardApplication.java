76f9ce4af05d73eb320f10839a43e5d58cbf5b92
Codeur47 <yoropapers@gmail.com>
2021-09-01 18:04:27 +0000

    First commit

---
+++ src/main/java/com/yorosoft/enoticeboard/ENoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.yorosoft.enoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENoticeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yorosoft/enoticeboard/ENoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorosoft.enoticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
